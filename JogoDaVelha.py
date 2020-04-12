9c7f639e564063790c369af51370f895cae29560
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-11 22:03:08 -0300

    Acrescentando inteligência artificial

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -43,7 +43,6 @@ def tabuleiro (c1, c2, c3, c4, c5, c6, c7, c8, c9):
     print()
     print('{:^50}'.format('=' * 50))
 
-
 def opcaoInicial():
     while True:
         boasvindas()
@@ -68,6 +67,18 @@ def opcaoPlayer():
     while opcao != '1' and opcao != '2':
         opcao = input('Opção inválida. Qual a sua opção? [1/2] ').strip()
     return opcao
+
+def modoDificil():
+    print('Deseja jogar em qual dificuldade?\n'
+          '[1] normal (movimentos aleatórios pc)\n'
+          '[2] difícil (movimentos do pc são calculados)')
+    opcao = input('Qual a sua opção? ').strip()
+    while opcao != '1' and opcao != '2':
+        opcao = input('Opção inválida. Qual a sua opção? [1/2] ').strip()
+    if opcao == '1':
+        return False
+    else:
+        return True
 
 def verificarVencedor(c1, c2, c3, c4, c5, c6, c7, c8, c9):
     #validando linhas
@@ -100,6 +111,175 @@ def verificarEmpate(c1, c2, c3, c4, c5, c6, c7, c8, c9):
     else:
         return True
 
+def nivelDificil(c1, c2, c3, c4, c5, c6, c7, c8, c9, pc):
+    if pc == 'o':
+        adv = 'x'
+    elif pc == 'x':
+        adv = 'o'
+    sugestao = 0
+    listaOpcoes1 = []
+    listaOpcoes2 = []
+    listaOpcoes3 = []
+    listaOpcoes4 = []
+    listaOpcoes5 = []
+    #lista sugestões 1
+    #vai sugerir casa 1
+    if c1 == ' ':
+        if c2 == pc and c3 == pc or c4 == pc and c7 == pc or c5 == pc and c9 == pc:
+            listaOpcoes1.append(1)
+    #vai sugerir casa 2
+    if c2 == ' ':
+        if c1 == pc and c3 == pc or c5 == pc and c8 == pc:
+            listaOpcoes1.append(2)
+    #vai sugerir casa 3
+    if c3 == ' ':
+        if c1 == pc and c2 == pc or c6 == pc and c9 == pc or c5 == pc and c7 == pc:
+            listaOpcoes1.append(3)
+    #vai sugerir casa 4
+    if c4 == ' ':
+        if c5 == pc and c6 == pc or c1 == pc and c7 == pc:
+            listaOpcoes1.append(4)
+    #vai sugerir casa 5
+    if c5 == ' ':
+        if c4 == pc and c6 == pc or c2 == pc and c8 == pc or c1 == pc and c9 == pc:
+            listaOpcoes1.append(5)
+    #vai sugerir casa 6
+    if c6 == ' ':
+        if c4 == pc and c5 == pc or c3 == pc and c9 == pc:
+            listaOpcoes1.append(6)
+    #vai sugerir casa 7
+    if c7 == ' ':
+        if c8 == pc and c9 == pc or c1 == pc and c4 == pc or c3 == pc and c5 == pc:
+            listaOpcoes1.append(7)
+    #vai sugerir casa 8
+    if c8 == ' ':
+        if c7 == pc and c9 == pc or c2 == pc and c5 == pc:
+            listaOpcoes1.append(8)
+    #vai sugerir casa 9
+    if c9 == ' ':
+        if c7 == pc and c8 == pc or c3 == pc and c6 == pc or c1 == pc and c5 == pc:
+            listaOpcoes1.append(9)
+    #lista sugestões 2
+    #vai sugerir casa 1
+    if c1 == ' ':
+        if c2 == adv and c3 == adv or c4 == adv and c7 == adv or c5 == adv and c9 == adv:
+            listaOpcoes2.append(1)
+    if c2 == ' ':
+        if c1 == adv and c3 == adv or c5 == adv and c8 == adv:
+            listaOpcoes2.append(2)
+    if c3 == ' ':
+        if c1 == adv and c2 == adv or c6 == adv and c9 == adv or c5 == adv and c7 == adv:
+            listaOpcoes2.append(3)
+    if c4 == ' ':
+        if c5 == adv and c6 == adv or c1 == adv and c7 == adv:
+            listaOpcoes2.append(4)
+    if c5 == ' ':
+        if c4 == adv and c6 == adv or c2 == adv and c8 == adv or c1 == adv and c9 == adv:
+            listaOpcoes2.append(5)
+    if c6 == ' ':
+        if c4 == adv and c5 == adv or c3 == adv and c9 == adv:
+            listaOpcoes2.append(6)
+    if c7 == ' ':
+        if c8 == adv and c9 == adv or c1 == adv and c4 == adv or c3 == adv and c5 == adv:
+            listaOpcoes2.append(7)
+    if c8 == ' ':
+        if c7 == adv and c9 == adv or c2 == adv and c5 == adv:
+            listaOpcoes2.append(8)
+    if c9 == ' ':
+        if c7 == adv and c8 == adv or c3 == adv and c6 == adv or c1 == adv and c5 == adv:
+            listaOpcoes2.append(9)
+    #lista sugestões 3
+    #vai sugerir casa 1
+    if c1 == ' ':
+        if c2 == pc and c3 == ' ' or c3 == pc and c2 == ' ':
+            listaOpcoes3.append(1)
+        if c4 == pc and c7 == ' ' or c7 == pc and c4 == ' ':
+            listaOpcoes3.append(1)
+        if c5 == pc and c9 == ' ' or c9 == pc and c5 == ' ':
+            listaOpcoes3.append(1)
+    #vai sugerir casa 2
+    if c2 == ' ':
+        if c1 == pc and c3 == ' ' or c3 == pc and c1 == ' ':
+            listaOpcoes3.append(2)
+        if c5 == pc and c8 == ' ' or c8 == pc and c5 == ' ':
+            listaOpcoes3.append(2)
+    #vai sugerir casa 3
+    if c3 == ' ':
+        if c1 == pc and c2 == ' ' or c2 == pc and c1 == ' ':
+            listaOpcoes3.append(3)
+        if c6 == pc and c9 == ' ' or c9 == pc and c6 == ' ':
+            listaOpcoes3.append(3)
+        if c5 == pc and c7 == ' ' or c7 == pc and c5 == ' ':
+            listaOpcoes3.append(3)
+    #vai sugerir casa 4
+    if c4 == ' ':
+        if c5 == pc and c6 == ' ' or c6 == pc and c5 == ' ':
+            listaOpcoes3.append(4)
+        if c1 == pc and c7 == ' ' or c7 == pc and c1 == ' ':
+            listaOpcoes3.append(4)
+    #vai sugerir casa 5
+    if c5 == ' ':
+        if c4 == pc and c6 == ' ' or c6 == pc and c4 == ' ':
+            listaOpcoes3.append(5)
+        if c2 == pc and c8 == ' ' or c8 == pc and c2 == ' ':
+            listaOpcoes3.append(5)
+        if c3 == pc and c7 == ' ' or c7 == pc and c3 == ' ':
+            listaOpcoes3.append(5)
+    #vai sugerir casa 6
+    if c6 == ' ':
+        if c4 == pc and c5 == ' ' or c5 == pc and c4 == ' ':
+            listaOpcoes3.append(6)
+        if c3 == pc and c9 == ' ' or c9 == pc and c3 == ' ':
+            listaOpcoes3.append(6)
+    #vai sugerir casa 7
+    if c7 == ' ':
+        if c8 == pc and c9 == ' ' or c9 == pc and c8 == ' ':
+            listaOpcoes3.append(7)
+        if c1 == pc and c4 == ' ' or c4 == pc and c1 == ' ':
+            listaOpcoes3.append(7)
+        if c3 == pc and c5 == ' ' or c5 == pc and c3 == ' ':
+            listaOpcoes3.append(7)
+    #vai sugerir casa 8
+    if c8 == ' ':
+        if c7 == pc and c9 == ' ' or c9 == pc and c7 == ' ':
+            listaOpcoes3.append(8)
+        if c2 == pc and c5 == ' ' or c5 == pc and c2 == ' ':
+            listaOpcoes3.append(8)
+    #vai sugerir casa 9
+    if c9 == ' ':
+        if c7 == pc and c8 == ' ' or c8 == pc and c7 == ' ':
+            listaOpcoes3.append(9)
+        if c3 == pc and c6 == ' ' or c6 == pc and c3 == ' ':
+            listaOpcoes3.append(9)
+        if c1 == pc and c5 == ' ' or c5 == pc and c1 == ' ':
+            listaOpcoes3.append(9)
+    #lista sugestões 4
+    #vai sugerir casa 5
+    if c5 == ' ':
+        if c1 == adv or c3 == adv or c7 == adv or c9 == adv:
+            listaOpcoes4.append(5)
+    #lista sugestões 5
+    #vai sugerir casas 1, 3, 7 ou 9
+    if c1 == ' ':
+        listaOpcoes5.append(1)
+    if c3 == ' ':
+        listaOpcoes5.append(3)
+    if c7 == ' ':
+        listaOpcoes5.append(7)
+    if c9 == ' ':
+        listaOpcoes5.append(9)
+    #vai sugerir um número com base na situação do jogo
+    if len(listaOpcoes1) > 0:
+        sugestao = listaOpcoes1[randint(0, len(listaOpcoes1)-1)]
+    elif len(listaOpcoes2) > 0:
+        sugestao = listaOpcoes2[randint(0, len(listaOpcoes2)-1)]
+    elif len(listaOpcoes3) > 0:
+        sugestao = listaOpcoes3[randint(0, len(listaOpcoes3)-1)]
+    elif len(listaOpcoes4) > 0:
+        sugestao = listaOpcoes4[randint(0, len(listaOpcoes4)-1)]
+    elif len(listaOpcoes5) > 0:
+        sugestao = listaOpcoes5[randint(0, len(listaOpcoes5)-1)]
+    return sugestao
 
 def regras():
     limparTela()
@@ -173,6 +353,7 @@ while True:
     if jogadores == '2':
         nomeP2 = input('Qual o nome do segundo jogador? ')
     else:
+        dificil = modoDificil()
         nomeP2 = 'Computador'
 
     while True:
@@ -219,7 +400,10 @@ while True:
 
         while True:
             if jogadores == '1' and proximoJogador == 'Computador':
-                jogada = str(randint(1, 9))
+                if dificil:
+                    jogada = str(nivelDificil(c1, c2, c3, c4, c5, c6, c7, c8, c9, simboloP2))
+                else:
+                    jogada = str(randint(1, 9))
             else:
                 jogada = input(f'É a vez do {proximoJogador}({proximoSimbolo}): ').strip().lower()
             if jogada == 'sair' or jogada == 'trocar' or jogada == 'zerar' or jogada == 'novo':
